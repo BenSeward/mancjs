d0a11a28d86cf3a76faa6404fc38f5f9adf98bd3
Ben Seward <ben.seward@googlemail.com>
2023-03-20 21:07:14 +0000

    feat: added api endpoints for future reference

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+  mancJSProfile: any;
+  mancJSEvents: any;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const mancJSProfile = await fetch("https://api.meetup.com/mancjs").then(
+    (response) => response.json()
+  );
+
+  const mancJSEvents = await fetch("https://api.meetup.com/mancjs/events").then(
+    (response) => response.json()
+  );
+
+  res.status(200).json({ name: "John Doe", mancJSProfile, mancJSEvents });
 }

ef683da627b7ed8a4cb73d9fd208f650717b79ec
Ben Seward <ben.seward@googlemail.com>
2023-03-20 20:16:08 +0000

    feat: removed boilerplate code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <Image
       width={200}
-      height={200}
+      height={80}
       src="/assets/mancjs-logo.svg"
       alt="mancjs logo"
       data-testid="header-logo-image"

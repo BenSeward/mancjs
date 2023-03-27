bb9cfaacbe79b48e81e8b6fa3a3dd8460916474b
Lukasz <lukaszprzybylka@gmail.com>
2023-03-27 18:21:23 +0100

    style: added container styles

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,9 +1,11 @@
+import styles from './Container.module.css'
+
 interface ContainerProps {
     children: JSX.Element | Array<JSX.Element>
 }
 
 export const Container = ( { children }: ContainerProps ) => {
   return (
-    <div data-testid="page-container">{ children }</div>
+    <div data-testid="page-container" className={ styles.container }>{ children }</div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Lato } from "next/font/google";
 import { Header } from "@/components/Header";
+import { Container } from "@/components/Container";
 
 const lato = Lato({
   subsets: ["latin"],
@@ -17,8 +18,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={lato.className}>
-        <Header />
-        <h1>Under Construction</h1>
+        <Container>
+          <Header />
+          <h1>Under Construction</h1>
+        </Container>
       </main>
     </>
   );

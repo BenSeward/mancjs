c91e07345f0d7aa1bba726c98b195a3268827eba
Lukasz <lukaszprzybylka@gmail.com>
2023-05-22 20:55:41 +0100

    feat: created footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./Footer.module.css"
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footerContainer}>Built with ☕️ by <a href="https://github.com/l-przybylka/">Lukasz Przybylka</a></footer>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { Container } from "@/components/Container";
 import { EventCard } from "@/components/EventCard";
 import { Section } from "@/components/Section";
+import { Footer } from "@/components/Footer";
 
 const lato = Lato({
   subsets: ["latin"],
@@ -19,16 +20,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={lato.className}>
         <Container>
           <Header />
+
           <Section header={'Welcome to Manc.JS'} paragraph={'We run Javascript & tech events every-other month around central Manchester.'} buttonText={'Join our next event!'} link={'/'}/>
+          
           <h2>Next event</h2>
           <EventCard thumbnailUrl="https://secure.meetupstatic.com/photos/event/9/1/9/3/clean_478597267.jpg" title="🎉 Manc.JS - 10th Birthday party 🎉" eventDate="Wednesday, 22nd March - " eventTime="6:00 PM GMT" location="Slalom offices,  M1 ABC" buttonText={'Join our next event!'} link={'/'}/>
+          
           <h2>Previous events</h2>
           <EventCard thumbnailUrl="https://secure.meetupstatic.com/photos/event/9/1/9/3/clean_478597267.jpg" title="🎉 Manc.JS - 10th Birthday party 🎉" eventDate="Wednesday, 22nd March - " eventTime="6:00 PM GMT" location="Slalom offices,  M1 ABC" buttonText={'See event details'} variant="secondary" link={'/'}/>
+          
           <Section header={'Get in touch'} paragraph={'We love hearing from our community. If you want to come to an event but have a question reach out to one of our organisers on Meetup.'} buttonText={'Get in touch'} link={'/'}/>
         </Container>
+        <Footer/>
       </main>
     </>
   );
